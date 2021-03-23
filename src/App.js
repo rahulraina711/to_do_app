1be39b1f98ab2b474ae49847deaf2125a92c6a35
Rahul Raina <rahulraina711@gmail.com>
2021-03-23 21:23:54 +0530

    added more functionality

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './app.scss';
 import Clock from "react-clock";
 import 'react-clock/dist/Clock.css';
 import AddIcon from '@material-ui/icons/Add';
+import ClearIcon from '@material-ui/icons/ClearAll';
 import { TextField } from '@material-ui/core';
 import ToDos from './Components/ToDo';
 
 function App() {
-  const [value, setValue] = useState(new Date());
-  const [textValue, setTextValue] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [value, setValue] = useState(new Date()); // clock time value
+  const [textValue, setTextValue] = useState(""); // controlledDOM element value
+  const [todoList, setTodoList] = useState([]);   // all the tasks are stored in this state
 
   useEffect(()=>{
+    checkLocalStorage();
     renderTasks();
   },[])
  
@@ -27,20 +29,42 @@ function App() {
   }, []);
 
   function addTask (){
-    console.log("clicked Add", textValue);
-    let tasks = [...todoList, textValue]
+    //console.log("clicked Add", textValue);
+    localStorage.setItem(textValue, false)
+    let tasks = [...todoList, {task:textValue, done: false}];
     setTodoList(tasks);
     setTextValue("");
   }
 
+  function checkLocalStorage(){
+    const taskList = [];
+    if(localStorage.length!==0){
+      for(let i=0; i< localStorage.length;i++){
+        const x = {
+          task :localStorage.key(i),
+          done :localStorage.getItem(localStorage.key(i))
+        }; //object end
+        taskList.push(x);
+      }// for end
+      setTodoList(taskList);
+
+    }
+  }
+
+  function clearAll(){
+    localStorage.clear();
+    setTodoList([]);
+  }
+
   function renderTasks () {
     return todoList.map((task,i)=>{
-      return <ToDos key={i} task={task}/>
+      return <ToDos key={i} task={task.task} done={task.done} />
     })
   }
  
   return (
     <div className="main-frame">
+      <button className="clear-all" onClick={clearAll}>Clear All<ClearIcon /></button>
       <div className="clock">
         <Clock value={value} />
       </div>
--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CheckIcon from '@material-ui/icons/Check';
 import DeleteIcon from '@material-ui/icons/Delete';
 
@@ -9,10 +9,39 @@ export default function ToDo (props) {
     const [done, setDone] = useState(false);
     const [color, setColor] = useState("write-task");
 
+    useEffect(()=>{
+        checkAttrs();
+    },[])
+
+    function deleteTask (){
+        localStorage.removeItem(props.task);
+        setColor("delete");
+    }
+
+    function checkAttrs(){
+        console.log(props.done);
+        if(props.done==="true"){
+            console.log("if here");
+            setDone(true);
+            setColor("write-task-done");
+        }
+        else if(props.done==="false"){
+            console.log("else here");
+            setDone(false);
+            setColor("write-task");
+        }        
+    }
+
+    function setAttrs(){
+        localStorage.setItem(props.task, true);
+        setDone(true);
+        setColor("write-task-done");
+    }
+
     return(
         <div className={color} >
-            <div className="text" onClick={()=>{setDone(true);setColor("write-task-done")}}>{props.task}</div>
-            <div className="done">{done ? <CheckIcon /> : <DeleteIcon />}</div> 
+            <div className="text" onClick={setAttrs}>{props.task}</div>
+            <div className="done" onClick={deleteTask}>{done ? <CheckIcon /> : <DeleteIcon />}</div> 
         </div>
     )
 }

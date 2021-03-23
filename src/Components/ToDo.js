170c6aad8a1c7fbaafd1dd4e5499a3cb7b191cf7
Rahul Raina <rahulraina711@gmail.com>
2021-03-23 18:56:45 +0530

    made UI better

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import CheckIcon from '@material-ui/icons/Check';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 import './todo.scss';
 
 export default function ToDo (props) {
 
     const [done, setDone] = useState(false);
+    const [color, setColor] = useState("write-task");
 
     return(
-        <div className="write-task" onClick={()=>{setDone(true)}}>
-            <div className="text">{props.task}</div>
-            <div className="done">{done && <CheckIcon />}</div> 
+        <div className={color} >
+            <div className="text" onClick={()=>{setDone(true);setColor("write-task-done")}}>{props.task}</div>
+            <div className="done">{done ? <CheckIcon /> : <DeleteIcon />}</div> 
         </div>
     )
 }
